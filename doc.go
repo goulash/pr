f52280fc59b53258b7d9ef2331d65d5bee70769e
Ben Morgan <neembi@googlemail.com>
2013-07-23 19:04:08 +0200

    Updating documention

--- doc.go
+++ doc.go
@@ -2,41 +2,46 @@
 // Use of this source code is governed by an MIT license
 // that can be found in the LICENSE file.
 
-//
-// Package pr implements functions for pretty printing of information.
-//
-// Example
-//
-//  import (
-//  	"bufio"
-//  	"flag"
-//  	"fmt"
-//  	"io"
-//  	"os"
-//  	"strings"
-//
-//  	"github.com/goulash/pr"
-//  )
-//
-//  func main() {
-//  	reader := bufio.NewReader(os.Stdin)
-//  	buffer := make([]string, 0, 32)
-//
-//  	for {
-//  		line, err := reader.ReadString('\n')
-//  		if err != nil {
-//  			if err != io.EOF {
-//  				fmt.Errorf("error: %s\n", err)
-//  			}
-//  			break
-//  		}
-//
-//  		buffer = append(buffer, strings.TrimSpace(line))
-//  	}
-//
-//  	width := flag.Int("width", -1, "width of the terminal")
-//  	flag.Parse()
-//
-//  	pr.PrintAutoGrid(buffer, *width)
-//  }
+/*
+Package pr implements functions for pretty printing of information.
+
+Currently, it is not in a usable state, but this will change very
+soon!
+
+Example
+
+	import (
+		"bufio"
+		"flag"
+		"fmt"
+		"io"
+		"os"
+		"strings"
+
+		"github.com/goulash/pr"
+	)
+
+	func main() {
+		reader := bufio.NewReader(os.Stdin)
+		buffer := make([]string, 0, 32)
+
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err != io.EOF {
+					fmt.Errorf("error: %s\n", err)
+				}
+				break
+			}
+
+			buffer = append(buffer, strings.TrimSpace(line))
+		}
+
+		width := flag.Int("width", -1, "width of the terminal")
+		flag.Parse()
+
+		pr.PrintAutoGrid(buffer, *width)
+	}
+
+*/
 package pr
